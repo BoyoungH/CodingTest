3c0f483e0a3e5ee422d0e78e5baf1431947e4eb5
BoyoungH <hyong0903@naver.com>
2023-12-31 18:00:57 +0900

    Q1: 암호해독

---
+++ java/벼락코딩테스트/Q1.java
@@ -0,0 +1,20 @@
+public class Q1 {
+    public static void main(String[] args) {
+       String[] text = {
+        "   + -- + - + -   ",
+        "   + --- + - +   ",
+        "   + -- + - + -   ",
+        "   + - + - + - +   "
+       };
+
+       solution(text);
+    }
+
+    public static void solution(String[] text) {
+        for (int i = 0; i < text.length; i++) {
+            int n = Integer.parseInt(text[i].strip().replace(" ", "").replace("+", "1").replace("-", "0"), 2);
+            text[i] = String.valueOf((char)n);
+            System.out.print(text[i]);
+        }
+    }
+}
