9d7ba59d9376ca71d6fbd1c74730175b9e8907a6
BoyoungH <hyong0903@naver.com>
2024-09-28 19:57:52 +0900

    Add: 최소직사각형.java

---
+++ java/프로그래머스 고득점 Kit/최소직사각형.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+
+        for(int i=0; i<sizes.length; i++){
+            if(sizes[i][0]>sizes[i][1]){
+                int tmp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = tmp;
+            }
+        }
+
+        int wmax=0;
+        int hmax=0;
+
+        for(int i=0; i<sizes.length; i++){
+            wmax = Math.max(sizes[i][0], wmax);
+            hmax = Math.max(sizes[i][1], hmax);
+        }
+
+        return wmax*hmax;
+    }
+}
+
+//둘이 비교 후 작으면 바꾸기
