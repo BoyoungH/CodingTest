687cbdb20e4d7ce01ed575e757a353c3214f1adf
BoyoungH <hyong0903@naver.com>
2024-10-01 00:50:44 +0900

    Add: 타겟넘버.java

---
+++ java/프로그래머스 고득점 Kit/타겟 넘버.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+
+        answer = dfs(0, 0, numbers, target);
+
+        return answer;
+    }
+
+    public int dfs(int depth, int sum, int[] numbers, int target){
+        if(depth == numbers.length){
+            if(target == sum){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        // +
+        int plus = dfs(depth+1, sum+numbers[depth], numbers, target);
+
+        //-
+        int minus = dfs(depth+1, sum-numbers[depth], numbers, target);
+
+        return plus + minus;
+    }
+}
+
+//재귀함수에 덧셈, 뺄셈 두가지 dfs
